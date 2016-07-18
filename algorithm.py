0c54639d5c555bf9cf67de1a43d2cbd6fc256672
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-07-18 12:07:23 +0200

    Improve offspring calculation

--- algorithm.py
+++ algorithm.py
@@ -50,13 +50,23 @@ class Evolution(object):
                 self.op.mutate(ind)
 
         # crossover
-        self.population = sorted(self.population, key=self.fit)
-        p1, p2 = self.population[:2]
-        c1, c2 = self.op.crossover(
-            copy.deepcopy(p1), copy.deepcopy(p2))
-        if not c1 is None and not c2 is None:
-            f1 = self.fit(c1)
-            f2 = self.fit(c2)
+        self.sort()
 
-            if min(f1, f2) > self.fit(self.population[-2]):
-                self.population[-2:] = c1, c2
+        offspring = []
+        lp = len(self.population) // 2
+        for _ in range(lp):
+            idx1 = idx2 = int(math.sqrt(random.randrange(lp**2)))
+            while idx1 == idx2:
+                idx2 = int(math.sqrt(random.randrange(lp**2)))
+
+            p1, p2 = self.population[idx1], self.population[idx2]
+            c1, c2 = self.op.crossover(
+                copy.deepcopy(p1), copy.deepcopy(p2))
+            if not c1 is None and not c2 is None:
+                offspring.append(c1 if self.op.fitness(c1) > self.fitness[idx1] else p1)
+                offspring.append(c2 if self.op.fitness(c2) > self.fitness[idx2] else p2)
+            else:
+                offspring.extend((p1, p2))
+
+        self.population[:] = offspring
+        self.sorted = False
