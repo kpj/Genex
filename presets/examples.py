492d886ff23472773979221e863670080ff6369a
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-02-05 20:11:52 +0100

    AddFitzHugh-Nagumo preset

--- presets/examples.py
+++ presets/examples.py
@@ -36,6 +36,51 @@ class LotkaVolterra(BasePreset):
             ])
         ]
 
+class FitzHughNagumo(BasePreset):
+    """ Spiking neuron model
+    """
+    def get_system(self):
+        def func(state, t):
+            v, w = state
+
+            a = .7
+            b = .8
+            tau = 12.5
+            I_ext = .5
+
+            return np.array([
+                v - v**3/3 - w + I_ext,
+                (v + a - b*w) / tau
+            ])
+        return func
+
+    def generate_base_individual(self):
+        return [
+            Individual(1, '({})+({})', [
+                Individual(1, '({})-({})', [
+                    Individual(1, '({})-({})', [
+                        Individual(1, 'VAR_0_', fix_coeff=True),
+                        Individual(1, '({})/({})', [
+                            Individual(1, 'VAR_0_**3', fix_coeff=True),
+                            Individual(random.uniform(1,8), '1')
+                        ])
+                    ], fix_coeff=True),
+                    Individual(1, 'VAR_1_', fix_coeff=True)
+                ], fix_coeff=True),
+                Individual(random.uniform(0,1), '1')
+            ], fix_coeff=True),
+            Individual(1, '({})/({})', [
+                Individual(1, '({})-({})', [
+                    Individual(1, '({})+({})', [
+                        Individual(1, 'VAR_0_', fix_coeff=True),
+                        Individual(random.uniform(0,1), '1')
+                    ], fix_coeff=True),
+                    Individual(random.uniform(0,1), 'VAR_1_')
+                ], fix_coeff=True),
+                Individual(random.uniform(7,18), '1')
+            ], fix_coeff=True)
+        ]
+
 class LorenzSystem(BasePreset):
     """ Chaotic Lorenz system
     """
